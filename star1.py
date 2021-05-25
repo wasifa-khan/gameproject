1156e1e351e31fa8b94451b0850823ad9c146cab
wasifa-khan <83424343+wasifa-khan@users.noreply.github.com>
2021-05-26 01:16:56 +0500

    Add files via upload

---
+++ star1.py
@@ -0,0 +1,17 @@
+class spot:
+    def __init__(self, x, y):
+        self.i = x
+        self.j = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.neighbors = []
+        self.previous = None
+        self.obs = False
+        self.closed = False
+        self.value = 1
+
+    def show(self, color, st):
+        if self.closed == False :
+            pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), st)
+            pygame.display.update()
